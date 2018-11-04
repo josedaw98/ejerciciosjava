db26f807c9e16e1d4b34e5bd4d829bdd894837ab
junio <junio@LAPTOP-Q6GBDQD4.Home>
2018-11-04 14:01:35 +0100

    mosaicos

--- ejercicioIniciacion/src/ejercicioIniciacion/DibujosAsteriscos.java
+++ ejercicioIniciacion/src/ejercicioIniciacion/DibujosAsteriscos.java
@@ -1,55 +1,67 @@
 package ejercicioIniciacion;
 
 public class DibujosAsteriscos {
-	
+
 	static void Volcan(int h) {
-		int asteriscos=2;
-		int y=1;
-		int total=(int)Math.pow(2, h)/2;
-		
-		
-		//LA PARTE COMENTADA ES OTRA FORMA DE ELABORARLO
-		//int total =1;
-		/*for(int j=0;j<h;j++) {
-			 total+=total;
-		}*/
-		
-		for(int i=0;i<h;i++) {
-			//int espacios=(total/2)-y;
-			int espacios=total-(asteriscos/2);
-			
-			for(int j=0;j<espacios;j++) {
+		int asteriscos = 2;
+		int y = 1;
+		int total = (int) Math.pow(2, h) / 2;
+
+		// LA PARTE COMENTADA ES OTRA FORMA DE ELABORARLO
+		// int total =1;
+		/*
+		 * for(int j=0;j<h;j++) { total+=total; }
+		 */
+
+		for (int i = 0; i < h; i++) {
+			// int espacios=(total/2)-y;
+			int espacios = total - (asteriscos / 2);
+
+			for (int j = 0; j < espacios; j++) {
 				System.out.print(" ");
 			}
-			for(int j=0;j<asteriscos;j++) {
+			for (int j = 0; j < asteriscos; j++) {
 				System.out.print("x");
 			}
 			System.out.println();
-			asteriscos+=asteriscos;
-			y+=y;
+			asteriscos += asteriscos;
+			y += y;
 		}
 
 	}
-	
-	 static void mosaico(int f, int c) {
-		for(int i=0;i<f;i++) {
+
+	static void mosaico(int f, int c) {
+		for (int i = 0; i < f; i++) {
 //			for(int j=0;j<c;j++) {
 //				if((j%2!=0 && i%2==0)||(j%2==0 && i%2!=0)) {
 //					System.out.print('*');
 //				}else {
 //					System.out.print(' ');
 //				}
-			for(int j=0;j<c;j++) {
-			
-					System.out.print((j%2!=0 && i%2==0)||(j%2==0 && i%2!=0)?"*":" ");
+			for (int j = 0; j < c; j++) {
+
+				System.out.print((j % 2 != 0 && i % 2 == 0) || (j % 2 == 0 && i % 2 != 0) ? "*" : " ");
 			}
 			System.out.println();
 		}
 	}
-	 
-	 static void tablero(int f , int c) {
-		 
-		 
-	 }
-	
+
+	static void tablero(int f, int c) {
+
+		for (int i = 0; i < f; i++) {//numero de filas
+			for (int h = 0; h < 3; h++) {//cpara realizar los cuadrados,hay que hacer la linea tres veces
+				for (int j = 0; j < c; j++) {//numero de columnas
+					if ((j % 2 != 0 && i % 2 == 0) || (j % 2 == 0 && i % 2 != 0)) {//para saber si poner asterisco o espacio en blanco
+						System.out.print("x x x");
+
+					} else {
+						System.out.print("     ");
+					}
+				}System.out.println();//salto de linea
+			}
+			System.out.println();//salto de linea
+		}
+
+	}
+
 }
--- ejercicioIniciacion/src/ejercicioIniciacion/PruebaDibujosAsteriscos.java
+++ ejercicioIniciacion/src/ejercicioIniciacion/PruebaDibujosAsteriscos.java
@@ -5,7 +5,8 @@ public class PruebaDibujosAsteriscos {
 	public static void main(String[] args) {
 		
 		//DibujosAsteriscos.Volcan(6);
-		DibujosAsteriscos.mosaico(4, 8);
+		//DibujosAsteriscos.mosaico(4, 8);
+		DibujosAsteriscos.tablero(8, 8);
 		
 
 	}
