ef8fed15d249cbb064c4bcebfaeeffca7003c2e9
jose ramon <joserrf98@gmail.com>
2018-11-13 10:25:43 +0100

    ejercicio4 inacabado

---
+++ EjerciciosCadenas/src/Ejercicio4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Escribe tu nif");
+		String dni = sc.nextLine();
+		int p_num = dni.substring(0, dni.length() - 1);
+		char letra_dni = (char) dni.length();
+		String c = "trwagmyfpdxbnjzsqvhlcke";
+		int v = p_num % 23;
+		char letra = c.charAt(v);
+		System.out.println(p_num);
+//		if(letra==letra_dni) {
+//			System.out.println("el dni= "+ dni+ "es valido");
+//		}else {
+//			System.out.println("el dni= "+ dni+ " no es valido");
+//		}
+
+	}
+
+}
