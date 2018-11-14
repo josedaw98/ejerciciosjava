3dcde97fecb5c03b100cc525df324bd234129421
jose ramon <joserrf98@gmail.com>
2018-11-14 12:21:10 +0100

    comprobacion

--- EjerciciosCadenas/src/Ejercicio4.java
+++ EjerciciosCadenas/src/Ejercicio4.java
@@ -6,17 +6,17 @@ public class Ejercicio4 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Escribe tu nif");
 		String dni = sc.nextLine();
-		int p_num = dni.substring(0, dni.length() - 1);
+		int p_num= ;
 		char letra_dni = (char) dni.length();
 		String c = "trwagmyfpdxbnjzsqvhlcke";
 		int v = p_num % 23;
 		char letra = c.charAt(v);
 		System.out.println(p_num);
-//		if(letra==letra_dni) {
-//			System.out.println("el dni= "+ dni+ "es valido");
-//		}else {
-//			System.out.println("el dni= "+ dni+ " no es valido");
-//		}
+		if(letra==letra_dni) {
+			System.out.println("el dni= "+ dni+ "es valido");
+		}else {
+			System.out.println("el dni= "+ dni+ " no es valido");
+		}
 
 	}
 
