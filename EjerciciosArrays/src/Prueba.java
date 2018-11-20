bc396e8fa612270c74d83b7b20767116aa5e9817
jose ramon <joserrf98@gmail.com>
2018-11-20 10:21:40 +0100

    primer array

---
+++ EjerciciosArrays/src/Prueba.java
@@ -0,0 +1,12 @@
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		
+		int [] x = { 47 ,31 ,2 ,5};
+		for(int e:x)//FOR EACH  e tiene el tamaño del array x
+		System.out.println( x[e] * 4);
+
+	}
+
+}
